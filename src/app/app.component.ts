922fd8b91b7831c40384926bbeb03719b5c8cf84
ThamodThisara <thamodthisara99@gmail.com>
2024-11-12 19:54:02 +0530

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageOfficerComponent } from './page/manage-officer/manage-officer.component';
 import { NavComponent } from './common/nav/nav.component';
+import { LoginPageComponent } from './page/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageOfficerComponent,NavComponent],
+  imports: [RouterOutlet,ManageOfficerComponent,NavComponent,LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,32 @@
 import { Routes } from '@angular/router';
 import { AddOfficerComponent } from './page/add-officer/add-officer.component';
 import { ManageOfficerComponent } from './page/manage-officer/manage-officer.component';
+import { LoginPageComponent } from './page/login-page/login-page.component';
+import { DashBoardPageComponent } from './page/dash-board-page/dash-board-page.component';
 
 export const routes: Routes = [
-    {path: "manage-officer", component: ManageOfficerComponent},
-    {path: "manage-officer",children: [
-        {path: "add-officer",component: AddOfficerComponent}
-    ]}
+    {
+        path: '',
+        component: LoginPageComponent
+    },
+    {
+        path: "dashboard",
+        component: DashBoardPageComponent,
+        children: [
+            {
+                path: "manage-officer",
+                component: ManageOfficerComponent
+            },
+            {
+                path: "manage-officer",
+                children: [
+                    {
+                        path: "add-officer", 
+                        component: AddOfficerComponent
+                    }
+                ]
+            }
+        ]
+    },
+
 ];
